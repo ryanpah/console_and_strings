306ce903e844414ecf51c0331bc7d339c3071a77
Ryan Page <rpage326@gmail.com>
2013-09-24 15:08:05 -0400

    Ryan's Story

---
+++ mad_libs.rb
@@ -0,0 +1,39 @@
+puts "it's bout time u gets 2 kno someone.."
+
+puts "Give a female name: "
+female_name = gets.chomp
+
+puts "Give an adjective: "
+adj1 = gets.chomp
+puts "Give an adjective: "
+adj2 = gets.chomp
+puts "Boo! Name a scary animal: "
+scary_animal = gets.chomp
+puts "Give me an adverb: "
+adv1 = gets.chomp
+puts "Give a past tense verb: "
+past_tense_verb = gets.chomp
+puts "Name an article of clothing: "
+article_of_clothing = gets.chomp
+puts "Give an adverb: "
+adv2 = gets.chomp
+puts "Name a body part: "
+body_part = gets.chomp
+puts "Give an emotion: "
+emotion = gets.chomp
+puts "Give a time: "
+time = gets.chomp
+puts "Last but not least, name a meal! "
+meal = gets.chomp
+puts "#{female_name} the painter had finished everything he needed to finish 
+that day and was walking around #{adj1}. Something #{adj2} caught
+his eye as he exited the elevator on the first floor of the Cambridge 
+Innovation Center. It was a General Assembly #{scary_animal}. He followed 
+#{adv1} behind but not too closely. He wondered why they were on the first
+floor today. As he entered this newly built part of the building he 
+#{past_tense_verb}. He pulled a paint-brush out of his #{article_of_clothing},
+walked into the room #{adv2} and gently caressed the hard yellow wall with
+his brush. The class couldn't see the tear drop fall from his #{body_part}, 
+but it happened. He was #{emotion}. It was time for his #{time} #{meal}."
+
+puts "I guess it's tru wut dey say. Vengeance iz a dishh best served cold BuurRR!"
